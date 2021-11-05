0d2135d36b52f61fb159a9f0702a706cb59dc7e1
Igor Zinar <igorzinar@gmail.com>
2021-11-05 07:03:09 +0200

    create errorMidleware.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,5 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
-import products from "../data/products.js";
 import Product from "../models/productModel.js";
 const router = express.Router();
 
@@ -26,7 +25,9 @@ router.get(
     if (product) {
       res.json(product);
     } else {
-      res.status(404).json({ message: "Product not found" });
+      res.status(404);
+      console.log(res.status);
+      throw new Error("Product not found");
     }
   })
 );
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
-import colors from "colors"
-import  connectDB  from "./config/db.js";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMidleware.js";
 
 import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
-connectDB()
+connectDB();
 
 const app = express();
 
@@ -16,11 +16,15 @@ app.get("/", (req, res) => {
   res.send("API is running....");
 });
 
-app.use("/api/products", productRoutes )
+app.use("/api/products", productRoutes);
 
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
 );
